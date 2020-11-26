3b13837b0bc23e6ab409e44eae1a62631866e218
ARTHARA <the.first678@gmail.com>
2020-11-26 20:32:40 +0700

    landing-not-fix

---
+++ app/src/main/java/com/katripstask/katrips/InputJadwalJmlhBayi.java
@@ -0,0 +1,14 @@
+package com.katripstask.katrips;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InputJadwalJmlhBayi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_jadwal_jmlh_bayi);
+    }
+}
---
+++ app/src/main/java/com/katripstask/katrips/InputJadwalJmlhDewasa.java
@@ -0,0 +1,14 @@
+package com.katripstask.katrips;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InputJadwalJmlhDewasa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_jadwal_jmlh_dewasa);
+    }
+}
