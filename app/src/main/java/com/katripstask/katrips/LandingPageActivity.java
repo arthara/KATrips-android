e35d99a9d7d5270f44c243733a6c6bcc61e31885
ARTHARA <the.first678@gmail.com>
2021-01-08 16:43:55 +0700

    landing-rev-not-fix

--- app/src/main/java/com/katripstask/katrips/LandingPageActivity.java
+++ app/src/main/java/com/katripstask/katrips/LandingPageActivity.java
@@ -9,6 +9,6 @@ public class LandingPageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing_page);
+        setContentView(R.layout.activity_landing_page_new);
     }
 }
